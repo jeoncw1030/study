6203205298593626c91810995c4b2c4259c08f25
jeoncw1030 <jeoncw1030@naver.com>
2018-01-06 17:47:05 +0900

    버블소트 완성

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,15 @@
-
-
+#import copy
 
 def bubble_sort(data):
-    pass
+    #change_data = copy.deepcopy(data)
+    ch_len = len(data)-1
+    
+    for i in range(ch_len):
+        for j in range(ch_len-i) :
+            if data[j] > data[j+1] :
+                temp = data[j]
+                data[j] = data[j+1]
+                data[j+1] = temp
 
 if __name__ == '__main__':
     data = [9, 8, 3, 6, 7]
