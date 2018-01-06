b96a0d3f0b5a15114ad501e6fbd8df14219dbe23
jeoncw1030 <jeoncw1030@naver.com>
2018-01-06 16:44:55 +0900

    버블소트 구현 전 테스트코드

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+
+
+
+def bubble_sort(data):
+    pass
+
+if __name__ == '__main__':
+    data = [9, 8, 3, 6, 7]
+
+    print(data) # [9, 8, 3, 6, 7]
+
+    bubble_sort(data)
+
+    print(data) # [3, 6, 7, 8, 9]
+    
